f06e326de1d76fd9d8d83a40037396307554f0b8
SinyukKirill <kirill.s.works@gmail.com>
2020-01-12 03:11:21 +0300

    /add command

--- src/main/java/commands/AddSpending.java
+++ src/main/java/commands/AddSpending.java
@@ -2,7 +2,6 @@ package commands;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.telegram.telegrambots.extensions.bots.commandbot.commands.ICommandRegistry;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
@@ -14,7 +13,7 @@ public class AddSpending extends PlannerBaseCommand {
     private static final Logger LOG = LoggerFactory.getLogger(HelpCommand.class);
     private final BotService botService;
 
-    public AddSpending(ICommandRegistry commandRegistry, BotService botService) {
+    public AddSpending(BotService botService) {
         super("add", "attributes:\n <category> <price> ", botService);
         this.botService = botService;
     }
@@ -22,16 +21,17 @@ public class AddSpending extends PlannerBaseCommand {
     public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
         LOG.info("User {}, id: {} is trying to execute '{}'.", user.getUserName(), user.getId(), getCommandIdentifier());
 
-        StringBuilder helpMessageBuilder = new StringBuilder();
+        StringBuilder addMessage = new StringBuilder();
 
         if(botService.hasAccessToCommands(user.getId())){
+            addMessage.append(String.format("Purchase %s with price %s successfully added.", arguments[1], arguments[2]));
             //add purchase to DB
         }
 
         SendMessage helpMessage = new SendMessage();
         helpMessage.setChatId(chat.getId().toString());
         helpMessage.enableHtml(true);
-        helpMessage.setText(helpMessageBuilder.toString());
+        helpMessage.setText(addMessage.toString());
 
         execute(absSender, helpMessage, chat, user);
     }
