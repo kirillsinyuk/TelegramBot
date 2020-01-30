9cf9e1e380b54e00282884c7e85bad96975be496
SinyukKirill <kirill.s.works@gmail.com>
2020-01-30 15:57:34 +0300

    commands refactoring

--- src/main/java/com/bot/commands/commonCommands/AddSpendingCommand.java
+++ src/main/java/com/bot/commands/commonCommands/AddSpendingCommand.java
@@ -4,7 +4,6 @@ import com.bot.commands.PlannerBaseCommand;
 import com.bot.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
@@ -23,21 +22,17 @@ public class AddSpendingCommand extends PlannerBaseCommand {
     public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
         LOG.info("BotUser {}, id: {}, chat: {} is trying to execute '{}'.", user.getUserName(), user.getId(), chat.getId(), getCommandIdentifier());
 
-        StringBuilder addMessage = new StringBuilder();
+        StringBuilder message = new StringBuilder();
 
         if(botService.hasAccessToCommands(user.getId())){
             if (arguments.length < 2) {
-                addMessage.append("You need to use this format:\n /add &lt;category&gt; &lt;price&gt; &lt;description&gt;");
+                message.append("You need to use this format:\n /add &lt;category&gt; &lt;price&gt; &lt;description&gt;");
             } else {
                 productService.createAndSaveProduct(arguments, user.getUserName());
-                addMessage.append(String.format("Purchase %s with price %s successfully added.", arguments[0], arguments[1]));
+                message.append(String.format("Purchase %s with price %s successfully added.", arguments[0], arguments[1]));
             }
         }
 
-        SendMessage helpMessage = new SendMessage();
-        helpMessage.setChatId(chat.getId().toString());
-        helpMessage.setText(addMessage.toString());
-
-        execute(absSender, helpMessage, chat, user);
+        sendMsg(absSender, user, chat, message.toString());
     }
 }
--- src/main/java/com/bot/service/ProductService.java
+++ src/main/java/com/bot/service/ProductService.java
@@ -3,9 +3,9 @@ package com.bot.service;
 import com.bot.model.dto.StatisticDto;
 import com.bot.model.entities.Product;
 import com.bot.repositories.ProductRepository;
+import com.bot.service.util.CalculateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -16,26 +16,33 @@ public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
-    @Autowired
-    private BotService botService;
 
-    @Transactional
+
     public Product createAndSaveProduct(String[] arguments, String username){
            Product product = toProduct(arguments, username);
            return productRepository.save(product);
     }
 
-    @Transactional
     public List<Product> getPurchases(LocalDateTime start, LocalDateTime end){
         return productRepository.getAllByDataBetween(start, end);
     }
 
-    @Transactional
-    public List<Object[]> getStatistic(LocalDateTime start, LocalDateTime end){
+    private List<Object[]> getStatistic(LocalDateTime start, LocalDateTime end){
         return productRepository.getStatistic(start, end);
     }
 
-    @Transactional
+    public String getStaticticMsg(LocalDateTime start, LocalDateTime end, BigDecimal total) {
+        StringBuilder str = new StringBuilder();
+        getStatistic(start, end)
+                .stream()
+                .map(this::toStatisticsDto)
+                .forEach(price -> str.append(price.toString())
+                        .append(" (")
+                        .append(CalculateUtils.getPercent(price.getPrice(), total))
+                        .append("%)\n"));
+        return str.toString();
+    }
+
     public BigDecimal totalSpend(LocalDateTime start, LocalDateTime end){
         return productRepository.getSum(start, end);
     }
