6d588858c6de3291927d451e2ae1ea40d742c67c
Кирилл Синюк <kirillsinyuk@Kirills-MacBook-Pro.local>
2023-03-09 22:20:45 +0400

    add core-api lib

---
+++ planner-core-api/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `maven-publish`
+    kotlin("jvm") version "1.7.22"
+}
+
+group = "com.kvsinyuk"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("planner-core-api") {
+            from(components["java"])
+        }
+    }
+}
